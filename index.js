e4ec69913e5427719127722bed15aaf8e4242a58
User Name <emelinda.gutierrez04@gmail.com>
2023-10-17 23:01:54 -0400

    Fixed bug

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const {} = require('./baseball')
+const { } = require('./baseball');
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Welcome to my Baseball App!'})
-})
+    return res.json({ message: 'Welcome to my Baseball App!' })
+});
 
 app.get('/favplayer', (req, res) => {
     return res.json({ answer: answer });
-})
+});
 
 app.get('/metsroster', (req, res) => {
-    return res.json({Roster: 'Jeff Brigham, José Butto, Carlos Carrasco, Sam Coonrod, Reed Garrett, Trevor Gott,Grant Hartwig,Anthony Kay, Joey Lucchesi, Tylor Megill, Vinny Nittoli, Adam Ottavino, David Peterson, José Quintana, Brooks Raley, Sean Reid-Foley,Denyi Reyes, Kodai Senga, Drew Smith'})
-})
+    return res.json({ Roster: 'Pitchers || Jeff Brigham, José Butto, Carlos Carrasco, Sam Coonrod, Reed Garrett, Trevor Gott,Grant Hartwig,Anthony Kay, Joey Lucchesi, Tylor Megill, Vinny Nittoli, Adam Ottavino, David Peterson, José Quintana, Brooks Raley, Sean Reid-Foley,Denyi Reyes, Kodai Senga, Drew Smith' })
+});
 
-fs.readFile('description.txt', 'utf8', (error, data) => {
+fs.readFile('about.txt', 'utf8', (error, data) => {
     if (error) {
         console.log('----- error ----', error);
     } else {
         console.log(data);
     }
-})
+});
 
 app.get('/read', (req, res) => {
     let element = req.query.something; // story
     fs.readFile(`${element}.txt`, 'utf8', (error, data) => {
         if (error) {
-            return res.json({ message: 'There is an issue, try again later...'});
+            return res.json({ message: 'There is an issue, try again later...' });
         } else {
             console.log(data)
             return res.json({ message: data });
@@ -38,4 +38,4 @@ app.get('/read', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server is running on PORT', PORT)
-})
+});
