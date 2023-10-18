af23569cd197e1b0c2417af452e0d51f5de9e835
User Name <emelinda.gutierrez04@gmail.com>
2023-10-17 22:14:32 -0400

    Initalized express and added file structure:

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+fs.readFile('description.txt', 'utf8', (error, data) => {
+    if (error) {
+        console.log('----- error ----', error);
+    } else {
+        console.log(data);
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on PORT', PORT)
+})
